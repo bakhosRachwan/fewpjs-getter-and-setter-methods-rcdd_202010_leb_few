1447565cf59ae2a314c4b55280deac469b1c4c63
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2020-12-11 21:56:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ class Circle{
     return Math.PI*(this.radius*this.radius);
   }
   set diameter(diameter){
-   this.diameter = diameter;
+   this.radius=(diameter/2);
+  }
+  set circumference(circumference){
+   this.radius=(circumference/(Math.PI*2));
+  }
+  set area(area){
+   this.radius=(Math. sqrt(area/Math.PI));
   }
 }
